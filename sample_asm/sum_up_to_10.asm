084dfcd634d21f4499adab6192e209125eef14ba
ryu.ishikawa <cvcl0808@gmail.com>
2020-08-24 01:08:36 +0900

    update sample

--- sample_asm/sum_up_to_10.asm
+++ sample_asm/sum_up_to_10.asm
@@ -1,17 +1,28 @@
+@10
+D=A;
 @i
-M=10;
-@sum=0;
+M=D;
+@sum
+M=0;
 (LOOP)
+    // sum = sum + i
     @sum
     D=M;
-    @i;
-    D=A+D;
+    @i
+    D=D+M;
     @sum
     M=D;
-    @i 
-    D=A;
+    // i = i - 1
+    @i
+    M=M-1;
+    D=M;
     @END
     D;JEQ
     @LOOP
     0;JMP
 (END)
+@sum
+D=M;
+(END_LOOP)
+@END_LOOP
+0;JMP
